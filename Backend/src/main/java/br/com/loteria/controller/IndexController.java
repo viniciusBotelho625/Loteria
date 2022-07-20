b33191cea74e18845e2e7e62e1bc3db23e6416c4
Vinicius Botelho <viniciusoficialbotelho@outlook.com>
2022-07-20 12:34:23 -0300

    design screens

---
+++ Backend/src/main/java/br/com/loteria/controller/IndexController.java
@@ -0,0 +1,16 @@
+package br.com.loteria.controller;
+
+import br.com.caelum.vraptor.Controller;
+
+@Controller
+public class IndexController {
+
+	
+	public IndexController() {
+		
+	}
+	
+	public void home() {
+		
+	}
+}
---
+++ Backend/src/main/java/br/com/loteria/controller/LoginController.java
@@ -0,0 +1,16 @@
+package br.com.loteria.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+@Path("login")
+@Controller
+public class LoginController {
+	
+	@Get("")
+	public void login() {
+		
+	}
+
+}
